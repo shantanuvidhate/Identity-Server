962d51e6e22b25a658d946bf75c7d96d797e3aa3
shantanuvidhate <shantanuvidhate00@gmail.com>
2023-07-11 13:33:16 +0530

    Project initialized

---
+++ identity-server/src/main/java/com/IdentityServer/identityserver/IdentityServerApplication.java
@@ -0,0 +1,13 @@
+package com.IdentityServer.identityserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentityServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdentityServerApplication.class, args);
+	}
+
+}
