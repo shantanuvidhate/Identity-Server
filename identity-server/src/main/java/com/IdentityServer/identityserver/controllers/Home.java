c6b3b8f5154f1cf8868d0b45b6b0456dbf33eb63
shantanuvidhate <shantanuvidhate00@gmail.com>
2023-07-11 15:18:06 +0530

    Autowired UserService

--- identity-server/src/main/java/com/IdentityServer/identityserver/controllers/Home.java
+++ identity-server/src/main/java/com/IdentityServer/identityserver/controllers/Home.java
@@ -1,17 +1,24 @@
 package com.IdentityServer.identityserver.controllers;
 
+import com.IdentityServer.identityserver.models.User;
+import com.IdentityServer.identityserver.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController             // for REST API endpoints
 @RequestMapping("/home")
 public class Home {
 
+    @Autowired                      // Autowiring create object at runtime
+    private UserService userService;
+
     @GetMapping ("/users")
-    public String getUsers (){
-        System.out.println("Getting users");
-        return "Users";
+    public List<User> getUsers (){
+        return this.userService.getUsers();
     }
 
 
