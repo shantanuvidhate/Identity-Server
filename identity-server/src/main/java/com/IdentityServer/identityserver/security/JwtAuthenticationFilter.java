9cb127bf96ad49c516cc0d538d57392f4f0128b0
shantanuvidhate <shantanuvidhate00@gmail.com>
2023-07-13 14:33:20 +0530

    logger implemented

---
+++ identity-server/src/main/java/com/IdentityServer/identityserver/security/JwtAuthenticationFilter.java
@@ -0,0 +1,31 @@
+package com.IdentityServer.identityserver.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private Logger logger = LoggerFactory.getLogger(OncePerRequestFilter.class);
+    @Autowired
+    private JwtHelper jwtHelper;
+
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+    }
+}
