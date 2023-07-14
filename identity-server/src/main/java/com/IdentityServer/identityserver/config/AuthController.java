9c98e511d8f774eb200c12ede16b8bdf152bebc1
shantanuvidhate <shantanuvidhate00@gmail.com>
2023-07-14 13:03:56 +0530

    Exception handling for AuthenticationManager

--- identity-server/src/main/java/com/IdentityServer/identityserver/config/AuthController.java
+++ identity-server/src/main/java/com/IdentityServer/identityserver/config/AuthController.java
@@ -1,4 +1,23 @@
 package com.IdentityServer.identityserver.config;
 
+
+import com.IdentityServer.identityserver.security.JwtHelper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
 public class AuthController {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtHelper helper;
 }
