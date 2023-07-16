db436a3d8095a75d44bd94449228a3ac42b60e23
shantanuvidhate <shantanuvidhate00@gmail.com>
2023-07-16 21:50:20 +0530

    In memory User removed

--- identity-server/src/main/java/com/IdentityServer/identityserver/services/UserService.java
+++ identity-server/src/main/java/com/IdentityServer/identityserver/services/UserService.java
@@ -1,24 +1,15 @@
 package com.IdentityServer.identityserver.services;
 
-import com.IdentityServer.identityserver.models.User;
+import com.IdentityServer.identityserver.entities.User;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 
 @Service
 public class UserService {
 
     private List<User> store = new ArrayList<>();
-
-//    Constructor
-    public UserService (){
-        store.add(new User(UUID.randomUUID().toString(),"Alice","Alice@gmail.com"));
-        store.add(new User(UUID.randomUUID().toString(),"Bob","Bob@gmail.com"));
-        store.add(new User(UUID.randomUUID().toString(),"Tom","Tom@gmail.com"));
-        store.add(new User(UUID.randomUUID().toString(),"Jerry","Jerry@gmail.com"));
-    }
 
 //    Return the store List
     public List<User> getUsers (){
