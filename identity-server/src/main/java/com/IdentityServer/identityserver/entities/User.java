3f797077f1ee2cc169cb85170f803c89928b71a8
shantanuvidhate <shantanuvidhate00@gmail.com>
2023-07-16 21:59:12 +0530

    User implements UserDetails

--- identity-server/src/main/java/com/IdentityServer/identityserver/entities/User.java
+++ identity-server/src/main/java/com/IdentityServer/identityserver/entities/User.java
@@ -2,9 +2,13 @@ package com.IdentityServer.identityserver.entities;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 
 @Document(collection = "user_credentials")
-public class User {
+public class User implements UserDetails {
 
     @Id
     private String userId;
@@ -34,11 +38,36 @@ public class User {
     }
 
     public String getUsername() {
-        return username;
+        return this.username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
     }
 
     public String getPassword() {
