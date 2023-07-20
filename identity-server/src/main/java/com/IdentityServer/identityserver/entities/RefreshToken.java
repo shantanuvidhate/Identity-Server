76fde6faf5d57fdef349b81648b8022adafe1b0b
shantanuvidhate <shantanuvidhate00@gmail.com>
2023-07-20 23:23:27 +0530

    Token variables

--- identity-server/src/main/java/com/IdentityServer/identityserver/entities/RefreshToken.java
+++ identity-server/src/main/java/com/IdentityServer/identityserver/entities/RefreshToken.java
@@ -1,4 +1,10 @@
 package com.IdentityServer.identityserver.entities;
 
+import java.time.Instant;
+
 public class RefreshToken {
+
+    private String refreshToken;
+
+    private Instant expiry;
 }
