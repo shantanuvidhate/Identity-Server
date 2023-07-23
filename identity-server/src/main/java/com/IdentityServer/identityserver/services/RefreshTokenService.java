e8815f894aaa111820a457469962821cc3cc5336
shantanuvidhate <shantanuvidhate00@gmail.com>
2023-07-23 21:33:43 +0530

    create reftreshtoken method

--- identity-server/src/main/java/com/IdentityServer/identityserver/services/RefreshTokenService.java
+++ identity-server/src/main/java/com/IdentityServer/identityserver/services/RefreshTokenService.java
@@ -1,4 +1,34 @@
 package com.IdentityServer.identityserver.services;
 
+import com.IdentityServer.identityserver.Repositories.RefreshTokenRepository;
+import com.IdentityServer.identityserver.entities.RefreshToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Service
 public class RefreshTokenService {
+
+    public long refreshTokenValidity = 5*60*60*1000;
+
+
+
+    @Autowired
+    private RefreshTokenRepository refreshTokenRepository;
+
+    public RefreshToken createRefreshToken (String userName){
+        RefreshToken refreshToken = RefreshToken.builder()
+                .refreshToken(UUID.randomUUID().toString())
+                .expiry(Instant.now().plusMillis(refreshTokenValidity))
+//                .user()
+                .build();  // 5hrs
+        return refreshToken;
+    }
+
+   public boolean verifyRefreshToken (String refreshToken){
+        return false;
+   }
+
 }
